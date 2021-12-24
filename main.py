565283b3479395d4ee63aa87867ed8e3936ec179
khadada <lkhadada@gmail.com>
2021-12-24 07:17:27 +0100

    I finish add the logo for this project

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ import tkinter
 main_window = Tk()
 main_window.title("Password generator")
 main_window.config(padx=20,pady=20)
+canvas = Canvas(main_window,height=200,width=200)
+canvas.grid(row=0,column=0,columnspan=3)
+img = PhotoImage(file='logo.png')
+canvas.create_image(100,100,image=img)
+
 
 
 main_window.mainloop()
