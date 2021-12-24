a7f642c5069a869f83beac155d8ccf1529b652e6
khadada <lkhadada@gmail.com>
2021-12-24 07:08:01 +0100

    I finish the main window setup

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from tkinter import *
+import tkinter
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+# main screen
+main_window = Tk()
+main_window.title("Password generator")
+main_window.config(padx=20,pady=20)
+
+
+main_window.mainloop()
