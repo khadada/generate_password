4f75935a9205b285bc502511b83364036fe1cb2f
khadada <lkhadada@gmail.com>
2022-01-02 19:52:58 +0100

    I add joind mechanism to the project

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
 from tkinter import *
 from tkinter import messagebox
-import random
+from random import randint, shuffle, choice
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 #Password Generator Project
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
-
-letter_password = [random.choice(letters) for _ in range(nr_letters)]
-symbole_password = [random.choice(symbols) for _ in range(nr_symbols)]
-number_password = [random.choice(numbers) for _ in range(nr_numbers)]
+randint(2, 4)
+letter_password = [choice(letters) for _ in range(randint(8, 10))]
+symbole_password = [choice(symbols) for _ in range(randint(2, 4))]
+number_password = [choice(numbers) for _ in range(randint(2, 4))]
 password_list = letter_password+symbole_password+number_password
-random.shuffle(password_list)
+shuffle(password_list)
 
-password = ""
-for char in password_list:
-  password += char
+password = "".join(password_list)
 
 print(f"Your password is: {password}")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
